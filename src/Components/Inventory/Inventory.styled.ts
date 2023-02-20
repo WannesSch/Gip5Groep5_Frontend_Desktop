7555e80ce70de538be72d3d05768035c4c996ff9
Aro0909 <ararat.gundes@student.ucll.be>
2023-02-20 19:17:22 +0100

    tabel

--- src/Components/Inventory/Inventory.styled.ts
+++ src/Components/Inventory/Inventory.styled.ts
@@ -1,1 +1,7 @@
+import styled from "@emotion/styled";
+import { Box } from "@mui/system";
 import React from "react";
+
+export const StyledTableBox = styled (Box)({
+    height: "40vh"
+})
--- src/Components/Inventory/Inventory.tsx
+++ src/Components/Inventory/Inventory.tsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import { Box, Button, TextField, textFieldClasses } from "@mui/material";
+import { StyledTableBox } from "./Inventory.styled";
+
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "name", headerName: "Naam", width: 130 },
+  { field: "type", headerName: "Type", width: 130 },
+  { field: "extraInformation", headerName: "Extra Informatie", width: 130 },
+  { field: "modelNumber", headerName: "Model Nummer", width: 140 },
+  { field: "price", headerName: "Prijs", width: 130 },
+  { field: "inStock", headerName: "Hoeveelheid", width: 130 },
+
+
+ 
+];
+
+const rows = [
+  { id: "", name: "", type: "", extraInformation: "", modelNumber:"", price:"", inStock: "" },
+
+];
 
 function InventoryComponent() {
-  return <>Inventory</>;
+  return (
+    <StyledTableBox >
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+       
+
+        
+      />
+    </StyledTableBox>
+  );
 }
 
 export default InventoryComponent;
