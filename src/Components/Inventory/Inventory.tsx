c43159a34a693b64ee296c797ea928a24683e929
Aro0909 <ararat.gundes@student.ucll.be>
2023-02-22 21:21:49 +0100

    AddRows

--- src/Components/Inventory/Inventory.tsx
+++ src/Components/Inventory/Inventory.tsx
@@ -1,29 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 import { Box, Button, TextField, textFieldClasses } from "@mui/material";
 import { StyledTableBox } from "./Inventory.styled";
+import { TableRow } from "../../Models/Props/TableRow";
 
 
 const columns: GridColDef[] = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "name", headerName: "Naam", width: 130 },
-  { field: "type", headerName: "Type", width: 130 },
-  { field: "extraInformation", headerName: "Extra Informatie", width: 130 },
-  { field: "modelNumber", headerName: "Model Nummer", width: 140 },
-  { field: "price", headerName: "Prijs", width: 130 },
-  { field: "inStock", headerName: "Hoeveelheid", width: 130 },
+  { field: "id", headerName: "ID", width: 70, editable:true },
+  { field: "name", headerName: "Naam", width: 130, editable:true },
+  { field: "type", headerName: "Type", width: 130, editable:true },
+  { field: "extraInformation", headerName: "Extra Informatie", width: 130, editable:true },
+  { field: "modelNumber", headerName: "Model Nummer", width: 140, editable:true },
+  { field: "price", headerName: "Prijs", width: 130, editable:true },
+  { field: "inStock", headerName: "Hoeveelheid", width: 130, editable:true },
 
 
  
 ];
 
-const rows = [
-  { id: "", name: "", type: "", extraInformation: "", modelNumber:"", price:"", inStock: "" },
+const tableRows:TableRow[] = [
+  { id: 0, name: "", type: "", extraInformation: "", modelNumber:"", price: 0, inStock: 0 },
 
 ];
 
+let idCounter = 0;
+const createRow = () => {
+  idCounter += 1;
+  return { id: idCounter, name: "", type: "", extraInformation: "", modelNumber:"", price: 0, inStock: 0 };
+};
+
+
 function InventoryComponent() {
-<<<<<<< HEAD
+
+   const [rows, setRows] = useState<TableRow[]>(tableRows);
+   
+const handleAddRow = () => {
+    setRows((rows) => [...rows, createRow()]);
+  };
+
+  
+
+  if(!rows) return<></>;
   return (
     <StyledTableBox >
       <DataGrid
@@ -33,15 +50,15 @@ function InventoryComponent() {
         pageSize={5}
         rowsPerPageOptions={[5]}
         checkboxSelection
-       
-
-        
+               
       />
+    <Box>
+      <Button onClick={handleAddRow}>Add</Button>
+  </Box>
     </StyledTableBox>
+    
   );
-=======
-  return <></>;
->>>>>>> 923ac9d7d411080dc1a7d1259aaccf6de6ffae22
+
 }
 
 export default InventoryComponent;
---
+++ src/Models/Props/TableRow.ts
@@ -0,0 +1,9 @@
+export interface TableRow{
+    id:number;
+    name: string;
+    type: string;
+    extraInformation:string;
+    modelNumber:string;
+    price:number;
+    inStock:number;
+}
